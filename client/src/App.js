6389d88749b8b930edda219a1165a46dab31e5b8
Melissa Longenberger <242424+melissa-24@users.noreply.github.com>
2020-04-22 23:06:44 -0400

    MVP and Stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +31,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Route exact path = "/">
+        <MovieList movies = {movieList} />
+      </Route>
+
+      <Route path = "/movies/:id">
+        <Movie addToSavedList = {addToSavedList} />
+      </Route>
+
     </div>
   );
 };
